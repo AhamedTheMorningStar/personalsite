0de9704d0dcd71b400e8efb1d1f379ca55a66f32
alpha <tarekahamedprime@gmail.com>
2023-11-29 11:20:16 -0800

    Updating Header & Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,10 +10,10 @@ import {BsInstagram} from 'react-icons/bs'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>L1V3D</a>
+      <a href="#top" className='footer__logo'>L1V3D</a>
 
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="#top">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#services">Certifications</a></li>
@@ -23,12 +23,12 @@ const Footer = () => {
       </ul>
 
       <div className='footer__socials'>
-        <a href="https://www.linkedin.com/in/tarekahamed007/" target="_blank" rel="opener"><BsLinkedin /></a>
-        <a href="https://github.com/TarekAhamed-LIV3D" target="_blank" rel="opener"><BsGithub /></a>
-        <a href="https://twitter.com/TarekAhamed777" target="_blank" rel="opener"><BsTwitter /></a>
-        <a href="https://www.facebook.com/tarekahamed666/" target="_blank" rel="opener"><BsFacebook /></a>
-        <a href="https://www.pinterest.com/thewerewolf007/" target="_blank" rel="opener"><BsPinterest /></a>
-        <a href="https://www.instagram.com/ahamed_the_morningstar/" target="_blank" rel="opener"><BsInstagram /></a>
+        <a href="https://www.linkedin.com/in/tarekahamed007/" rel="reopener"><BsLinkedin /></a>
+        <a href="https://github.com/TarekAhamed-LIV3D" rel="opener"><BsGithub /></a>
+        <a href="https://twitter.com/TarekAhamed777" rel="opener"><BsTwitter /></a>
+        <a href="https://www.facebook.com/tarekahamed666/" rel="opener"><BsFacebook /></a>
+        <a href="https://www.pinterest.com/thewerewolf007/" rel="opener"><BsPinterest /></a>
+        <a href="https://www.instagram.com/ahamed_the_morningstar/" rel="opener"><BsInstagram /></a>
       </div>
 
       <div className='footer__copyright'>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,12 +9,12 @@ import {BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://www.linkedin.com/in/tarekahamed007/" target="_blank" rel="opener"><BsLinkedin /></a>
-        <a href="https://github.com/TarekAhamed-LIV3D" target="_blank" rel="opener"><BsGithub /></a>
-        <a href="https://twitter.com/TarekAhamed777" target="_blank" rel="opener"><BsTwitter /></a>
-        <a href="https://www.facebook.com/tarekahamed666/" target="_blank" rel="opener"><BsFacebook /></a>
-        <a href="https://www.pinterest.com/thewerewolf007/" target="_blank" rel="opener"><BsPinterest /></a>
-        <a href="https://www.instagram.com/ahamed_the_morningstar/" target="_blank" rel="opener"><BsInstagram /></a>
+        <a href="https://www.linkedin.com/in/tarekahamed007/" rel="opener"><BsLinkedin /></a>
+        <a href="https://github.com/TarekAhamed-LIV3D" rel="opener"><BsGithub /></a>
+        <a href="https://twitter.com/TarekAhamed777" rel="opener"><BsTwitter /></a>
+        <a href="https://www.facebook.com/tarekahamed666/" rel="opener"><BsFacebook /></a>
+        <a href="https://www.pinterest.com/thewerewolf007/" rel="opener"><BsPinterest /></a>
+        <a href="https://www.instagram.com/ahamed_the_morningstar/" rel="opener"><BsInstagram /></a>
     </div>
   )
 }
